e4d4493da3ecd667e36434c36e310885ee5664a0
dhaval_sundhesha0059 <dhavalsundhesha@gmail.com>
2025-08-15 13:48:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// Root folder से static files serve करो
+// Root folder से सभी static files serve करो
 app.use(express.static(__dirname));
 
 // Root URL पर index.html भेजो
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Optional: किसी भी unknown route पर redirect करो /
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
