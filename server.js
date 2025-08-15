d13cf70e0edc754793d6c1aedefedcaa4fc00ff1
dhaval_sundhesha0059 <dhavalsundhesha@gmail.com>
2025-08-15 13:18:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-// server.js
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
+const path = require("path");
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-// Static files serve करो
-app.use(express.static(path.join(__dirname, 'public')));
+// Public folder serve करो
+app.use(express.static(path.join(__dirname, "public")));
 
-// Root URL पर index.html serve करो
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
